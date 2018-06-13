64584a8b3d138fd4d743afd39dc7f55dc37468b8
rekhagowda <rekhasampangi13@gmail.com>
2018-06-12 19:22:06 -0500

    Added home module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'Health Care Management';
+  title = 'Health Care Management'; 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './home/login/login.component';
+import { HomeModule } from './home/home.module';
+import { PatientModule } from './patient/patient.module';
+
+const appRoutes: Routes = [
+  { path: 'home', component: LoginComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    HomeModule,
+    PatientModule,
+    RouterModule.forRoot(appRoutes, { useHash: true })
+  ],
+  exports: [
+ 
+  ],
+  providers: [
+    //HomeComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
+
+
+
+
+
+
+
+
+/* import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { PrescriptionComponent } from './prescription/prescription.component';
 import { PatientModule } from './patient/patient.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -23,3 +73,4 @@ import { PatientModule } from './patient/patient.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ */
