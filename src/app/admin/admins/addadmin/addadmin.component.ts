583631dafe80fe65fdf81bc1b7d7ca9fd35670c9
ashasinghp22 <ashasinghp22@gmail.com>
2018-06-16 01:12:25 -0500

    added admin front end functionality

--- src/app/admin/admins/addadmin/addadmin.component.ts
+++ src/app/admin/admins/addadmin/addadmin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ValidateService } from '../../../services/validate.service';
+import { FlashMessagesService} from 'angular2-flash-messages';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-addadmin',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddadminComponent implements OnInit {
 
-  constructor() { }
+  username:String;
+  firstname:String;
+  lastname:String;
+  email:String;
+  password:String;
+  retypepassword:String;
+  contact:String;
+
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
   }
+
+  onAddAdmin(){
+    const admin = {
+      username: this.username,
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+      password: this.password,
+      retypepassword: this.retypepassword,
+      contact: this.contact
+
+    }
+
+    if(!this.validateService.validateRegister(admin)){
+      this.flashMessage.show("Please fill in all fields", {cssClass: 'alert-danger', timeout:5000});
+       return false;
+     }
+  
+     if(!this.validateService.validateEmail(admin.email)){
+       this.flashMessage.show("Please enter valid email", {cssClass: 'alert-danger', timeout:4000});
+       return false;
+     }
+
+     //Register User
+    this.authService. registerUser(admin).subscribe(data => {
+      if(data.success){
+       this.flashMessage.show("Registered new Admin ", {cssClass: 'alert-success', timeout:3000});
+       this.router.navigate(['/viewAdmin']);
+      } else {
+       this.flashMessage.show("Something went wrong", {cssClass: 'alert-danger', timeout:3000});
+       this.router.navigate(['/addAdmin']);
+      }
+   });
+  }
+
+ 
 
 }
--- src/app/admin/admins/admins.module.ts
+++ src/app/admin/admins/admins.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import {FlashMessagesModule } from 'angular2-flash-messages';
 import { AddadminComponent } from './addadmin/addadmin.component';
 import { ViewadminComponent } from './viewadmin/viewadmin.component';
 
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    FlashMessagesModule
   ],
   declarations: [
     AddadminComponent,
